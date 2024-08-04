342ff5e2ed33a62cca92911bb0d40a3c572cc43d
Juan Fahrul Fauzan <juanfhrul08@gmail.com>
2024-08-04 21:49:37 +0700

    register client module MQTT

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { EventModule } from './events/events.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'MQTT',
+        transport: Transport.MQTT,
+        options: {
+          url: 'http://broker.hivemq.com',
+          port: 1883
+        }
+      }
+    ]),
+    EventModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
