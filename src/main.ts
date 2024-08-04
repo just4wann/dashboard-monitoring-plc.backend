96c05a1d6fe92f134fcc273575cf2d7739f22df9
Juan Fahrul Fauzan <juanfhrul08@gmail.com>
2024-08-04 21:50:34 +0700

    create MQTT transport microservices

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // const app = await NestFactory.create(AppModule)
+  // app.connectMicroservice({
+  //   transport: Transport.MQTT,
+  //   options: {
+  //     url: 'http://broker.hivemq.com',
+  //     port: 1883    
+  //   }
+  // });
+  // app.startAllMicroservices();
+  // app.listen(3001);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.MQTT,
+    options: {
+      url: 'http://broker.hivemq.com',
+      port: 1883
+    }
+  })
 }
 bootstrap();
